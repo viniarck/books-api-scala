653b7f7070f51e8b378b09922189811a1227ea2d
Vinicius Arcanjo <viniarck@gmail.com>
2020-02-02 15:33:22 -0300

    Added Serializers.scala

---
+++ src/main/scala/io/github/viniarck/books/Serializers.scala
@@ -0,0 +1,25 @@
+package io.github.viniarck.books
+
+import io.getquill._
+import java.time.{LocalDateTime, ZoneId, ZoneOffset}
+import java.util.Date
+import io.github.viniarck.books.Models._
+
+object Serializers {
+
+  case class PersonSerializer(
+      name: String
+  )
+
+  case class LibrarySerializer(
+      name: String
+  )
+
+  case class BookSerializer(
+      title: String,
+      library_uuid: String,
+      author_uuid: String,
+      book_category: Option[String],
+      published_at: Option[LocalDateTime]
+  )
+}
